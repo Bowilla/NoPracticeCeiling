47f9d1e7c62a94c147c15b2ec2184b61dd40def0
Bowilla <josiahs10@gmail.com>
2025-10-17 21:20:06 -0500

    Add project files.

---
+++ NoPracticeCeilingMod.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using MelonLoader;
+
+namespace NoPracticeCeiling
+{
+    public class NoPracticeCeilingMod : MelonMod
+    {
+
+
+    }
+
+    [HarmonyPatch(typeof(PlayerController), nameof(PlayerController.LimitPracticeHeight))]
+    public class Patch
+    {
+        static bool Prefix(PlayerController __instance)
+        {
+            __instance.practiceModeBarrier.SetActive(false);
+            return false;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using NoPracticeCeiling;
+
+[assembly: MelonInfo(typeof(NoPracticeCeilingMod), "NoPracticeCeiling", "1.0.0", "Bowilla")]
+[assembly: MelonGame("", "Beton Brutal")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoPracticeCeiling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoPracticeCeiling")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51de1077-663d-4c86-a0db-2fd5cf5b2a39")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
